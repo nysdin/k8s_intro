75e127e20c797af67f5ee74c75d3159c3d1ee41f
nysdin <40791830+nysdin@users.noreply.github.com>
2023-04-02 02:24:36 +0900

    一括実行

---
+++ the-hard-way/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+echo $SCRIPT_DIR
+
+# FIXME: ディレクトリ移動しなくてもいいような書き方にしてほしい
+cd $SCRIPT_DIR
+cd certificate && ./setup.sh
+cd $SCRIPT_DIR
+cd configuration && ./setup.sh
+cd $SCRIPT_DIR
+cd secrets && ./setup.sh
+cd $SCRIPT_DIR
+cd etcd && ./setup.sh
+cd $SCRIPT_DIR
+cd control && ./setup.sh
