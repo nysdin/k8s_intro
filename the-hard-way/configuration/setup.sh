3fb515b301f24a93324d8f8ce69de4daa498afd9
nysdin <40791830+nysdin@users.noreply.github.com>
2023-04-02 17:21:20 +0900

    setup local kubeconfig

--- the-hard-way/configuration/setup.sh
+++ the-hard-way/configuration/setup.sh
@@ -125,3 +125,27 @@ echo "------------------------"
 for instance in ${controllers[@]}; do
   gcloud compute scp kube-scheduler.kubeconfig kube-controller-manager.kubeconfig admin.kubeconfig ubuntu@${instance}:~/
 done
+
+
+echo "------------------------"
+echo "作業用PC用の kubeconfig 設定"
+echo "------------------------"
+
+KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-hard-way \
+  --region $(gcloud config get-value compute/region) \
+  --format 'value(address)')
+
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.pem \
+  --embed-certs=true \
+  --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443
+
+kubectl config set-credentials admin \
+  --client-certificate=admin.pem \
+  --client-key=admin-key.pem
+
+kubectl config set-context kubernetes-the-hard-way \
+  --cluster=kubernetes-the-hard-way \
+  --user=admin
+
+kubectl config use-context kubernetes-the-hard-way
